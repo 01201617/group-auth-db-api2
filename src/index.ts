c2426a94e1ddfec8d9f0b12cba7645f32f4579e8
nan <torako.04saibino4ban@gmail.com>
2024-08-04 17:22:44 +0900

    swaggerのAPI仕様書を追加(今回はDataのみ)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
+import bodyParser from "body-parser";
 import express from "express";
 import { AppDataSource } from "./data-source";
 import dataRoutes from './routes/dataRoutes';
 import dataTypeRoutes from './routes/dataTypeRoutes';
 import groupRoutes from './routes/groupRoutes';
 import userRoutes from './routes/userRoutes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(bodyParser.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/data', dataRoutes);
 app.use('/data-types', dataTypeRoutes);
 app.use('/groups', groupRoutes);
